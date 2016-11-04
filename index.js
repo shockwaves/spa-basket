332d3ae69fc3c49878f0c28caca212c13f0b4585
Artem <futucoder@gmail.com>
2016-11-04 22:25:45 +0200

    test

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-/**
- * Created by futuc on 30.10.2016.
- */
 var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
-}).listen(process.env.PORT || 3000);var static = require('node-static');
 var file = new static.Server();
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
